e334172b6b7f752b612297e34e5e474bd6cb59a8
Usman Munir <usmanmunir1008@gmail.com>
2024-12-09 18:33:43 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Input from "./components/input"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold">Weather Dashboard</h1>
+      <Input/>
+     
+    </div>
+    
+    </>
+    
+  )
+}
+
+export default App
